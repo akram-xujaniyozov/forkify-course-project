e509d8c9e679e67e83f84d2b53ffc11b7f51ae4f
akram-xujaniyozov <akrom2661@gmail.com>
2023-06-01 20:09:16 +0500

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,10 +11,6 @@ import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 import { MODAL_CLOSE_SEC } from './config.js';
 
-if (module.hot) {
-  module.hot.accept();
-}
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -49,7 +45,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render search results
-    // resultView.render(model.state.search.results);
+
     resultView.render(model.getSearchResultPage());
 
     // 4) Render initial pagination buttons
@@ -72,7 +68,7 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  // recipeView.render(model.state.recipe);
+
   recipeView.update(model.state.recipe);
 };
 
